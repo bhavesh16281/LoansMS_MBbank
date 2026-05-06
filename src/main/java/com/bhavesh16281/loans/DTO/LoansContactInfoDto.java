a6c3287afd8778622e885653a4181248e5181cdc
Muthyalu Bhavesh <66527078+bhavesh16281@users.noreply.github.com>
2026-05-06 11:00:36 +0530

    Added actuator to change config properties without restarting microservice

--- src/main/java/com/bhavesh16281/loans/DTO/LoansContactInfoDto.java
+++ src/main/java/com/bhavesh16281/loans/DTO/LoansContactInfoDto.java
@@ -1,11 +1,17 @@
 package com.bhavesh16281.loans.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String,String> contactDetails) {
+@Getter
+@Setter
+public class LoansContactInfoDto{
 
+    private String message;
+    private Map<String,String> contactDetails;
 }
 
