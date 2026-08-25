ceb364bcd110f1a6f6584b70a1dbac6e55143eed
Muthyalu Bhavesh <66527078+bhavesh16281@users.noreply.github.com>
2026-08-25 20:25:52 +0530

    Started Implementing API Gateway

--- src/main/java/com/bhavesh16281/loans/controller/LoansController.java
+++ src/main/java/com/bhavesh16281/loans/controller/LoansController.java
@@ -14,6 +14,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -32,6 +35,7 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 public class LoansController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoansController.class);    
     private final LoansService loansService;
 
     @Value("${build.version}")
@@ -133,9 +137,11 @@ public class LoansController {
     }
     )
     @GetMapping("/fetch")
-    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestHeader("MBBank-correlation-id") String correlationId, @RequestParam
                                                      @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                      String mobileNumber) {
+        logger.info("Fetching loan details for mobile number: {}", mobileNumber);
+        logger.info("Correlation ID: {}", correlationId);
         LoansDto loansDto = loansService.fetchLoan(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(loansDto);
     }
